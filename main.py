7eea9fea7bc1784d9ab5155d9931639d8d585fbd
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-02-06 15:07:52 +0300

    Add entry text class

--- main.py
+++ main.py
@@ -7,6 +7,54 @@ pg.init()
 pg.display.set_caption('Physic Project')
 
 
+class Entry_parameters:
+    def __init__(self, x, y, width, height, text, mess_x, mess_y):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.text_x = mess_x
+        self.text_y = mess_y
+        self.bg_color = (94, 94, 94)
+        self.need_input = False
+        self.font_color = (0, 247, 255)
+        self.active_line_color = (26, 26, 176)
+        self.font_size = 30
+        self.font_type = 'Arial'
+        self.line_color = (0, 0, 0)
+
+    def draw(self):
+        click = pg.mouse.get_pressed()
+        mouse = pg.mouse.get_pos()
+        if click[0] == 1 and self.x < mouse[0] < self.x + self.width and self.y < mouse[1] < self.y + self.height:
+            self.need_input = True
+        if click[0] == 1 and not (
+                self.x < mouse[0] < self.x + self.width and self.y < mouse[1] < self.y + self.height):
+            self.need_input = False
+        rect_1 = (self.x, self.y, self.width, self.height)
+        rect = pg.draw.rect(display, self.bg_color, rect_1)
+        if self.need_input:
+            left_line = pg.draw.line(display, self.active_line_color, [self.x, self.y], [self.x, self.y + self.height],
+                                     6)
+            top_line = pg.draw.line(display, self.active_line_color, [self.x, self.y], [self.x + self.width, self.y],
+                                    6)
+            down_line = pg.draw.line(display, self.active_line_color, [self.x, self.y + self.height],
+                                     [self.x + self.width, self.y + self.height], 6)
+            right_line = pg.draw.line(display, self.active_line_color, [self.x + self.width, self.y],
+                                      [self.x + self.width,
+                                       self.y + self.height], 6)
+        else:
+            left_line = pg.draw.line(display, self.line_color, [self.x, self.y], [self.x, self.y + self.height], 8)
+            top_line = pg.draw.line(display, self.line_color, [self.x, self.y], [self.x + self.width, self.y], 8)
+            down_line = pg.draw.line(display, self.line_color, [self.x, self.y + self.height],
+                                     [self.x + self.width, self.y + self.height], 8)
+            right_line = pg.draw.line(display, self.line_color, [self.x + self.width, self.y],
+                                      [self.x + self.width, self.y + self.height], 8)
+        text_font = Font(self.text_x, self.text_y, self.font_color, self.font_size, self.font_type, self.text)
+        text_font.draw_text()
+
+
 class Circle:
     def __init__(self, color, fill=False, pos=(400, 300)):
         self.color = color
